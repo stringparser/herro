ed859c26b45c4d2894aa9f83c75fbe933b2abedb
Javier Carrillo <stringparser@gmail.com>
2015-06-09 19:47:09 +0200

    dev: rewrite

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-module.exports = require('./lib/herro');
+'use strict';
+
+var util = require('./lib/util');
+
+
+function Herror(message){
+  if(!(this instanceof Herror)){
+    return new Herror(message);
+  }
+
+  this.cwd = process.cwd();
+  this.date = new Date().toISOString();
+  this.engine = 'node-' + process.version;
+  this.message = message;
+
+  Error.captureStackTrace(this,  Herror);
+
+  this.stack = 'cwd ' + this.cwd +
+    '\n\n' + util.npmize(this.stack) +
+    '\n\n' + this.engine + ' ' + this.date;
+}
+util.inherits(Herror, Error);
+
+exports = module.exports = Herror;
